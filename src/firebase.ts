4abef2d27cce33d56f1a7234ec721ebf34238dc0
haru507 <53287331+haru507@users.noreply.github.com>
2021-09-03 14:50:12 +0900

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,7 @@ import 'firebase/firebase-firestore'
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAiCepbUY_yCc5GP5IDdJOL_qgLOU-C9aE",
-    authDomain: "react-typescript-sample.firebaseapp.com",
-    projectId: "react-typescript-sample",
-    storageBucket: "react-typescript-sample.appspot.com",
-    messagingSenderId: "143751660017",
-    appId: "1:143751660017:web:ca3a9b67188f8c8a768450",
-    measurementId: "G-1S0TE0FW6R"
+    
   };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
